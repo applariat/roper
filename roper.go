5a5908349ea32873e12d508d5f8183d3e50f2776
Chris Dornsife <chris@dornsife.com>
2017-04-24 12:57:23 -0700

    initial commit

---
+++ roper.go
@@ -0,0 +1,96 @@
+package roper
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/ghodss/yaml"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"strings"
+)
+
+// Unmarshal ...
+func Unmarshal(input string, result interface{}) error {
+
+	if input == "" {
+		return errors.New("Missing input")
+	}
+
+	bytes, err := getBytesFromInput(input)
+
+	if err != nil {
+		return err
+	}
+
+	if bytes == nil {
+		return errors.New("File contained no data")
+	}
+
+	ext := path.Ext(input)
+
+	switch strings.ToLower(ext) {
+
+	case ".yaml", ".yml":
+		err = yaml.Unmarshal(bytes, &result)
+
+	case ".json":
+		err = json.Unmarshal(bytes, &result)
+
+	case ".toml":
+		err = toml.Unmarshal(bytes, &result)
+
+	default:
+		err = fmt.Errorf("Unsupported file type: %s", ext)
+	}
+
+	return err
+}
+
+// getBytesFromInput read info from STDIN, URL or from a file.
+func getBytesFromInput(inputFile string) ([]byte, error) {
+
+	switch {
+
+	case inputFile == "-":
+		fi, err := os.Stdin.Stat()
+		if err != nil {
+			return nil, err
+		}
+
+		if fi.Mode()&os.ModeNamedPipe != 0 {
+			return ioutil.ReadAll(os.Stdin)
+		}
+		return nil, errors.New("Unknown STDIN type")
+
+	case strings.Index(inputFile, "http://") == 0 || strings.Index(inputFile, "https://") == 0:
+		_, err := url.Parse(inputFile)
+		if err != nil {
+			return nil, err
+		}
+
+		resp, err := http.Get(inputFile)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		return ioutil.ReadAll(resp.Body)
+
+	default:
+		bytes, err := ioutil.ReadFile(inputFile)
+		if err != nil {
+			return nil, err
+		}
+		if bytes == nil {
+			return nil, errors.New("File empty")
+		}
+
+		return bytes, err
+	}
+
+}
---
+++ roper_test.go
@@ -0,0 +1,69 @@
+package roper_test
+
+import (
+	"github.com/cdornsife/roper"
+	"reflect"
+	"testing"
+)
+
+var controlTestStruct *TestStruct
+
+func init() {
+	controlTestStruct = &TestStruct{
+		One: 1,
+		Two: "two",
+	}
+
+	controlTestStruct.Three = append(controlTestStruct.Three, struct {
+		A string `json:"a"`
+		B string `json:"b"`
+		C string `json:"c"`
+	}{"a", "b", "c"})
+
+}
+
+// TestStruct represents the data in the file
+type TestStruct struct {
+	One   int    `json:"one"`
+	Two   string `json:"two"`
+	Three []struct {
+		A string `json:"a"`
+		B string `json:"b"`
+		C string `json:"c"`
+	} `json:"three"`
+}
+
+// TestRoper_JSON load and test tests/test.json
+func TestRoper_JSON(t *testing.T) {
+	doIt("tests/test.json", t)
+}
+
+// TestRoper_YAMLFile load and test tests/test.yaml
+func TestRoper_YAML(t *testing.T) {
+	doIt("tests/test.yaml", t)
+}
+
+// TestRoper_YMLFile load and test tests/test.yml
+func TestRoper_YML(t *testing.T) {
+	doIt("tests/test.yml", t)
+}
+
+// TestRoper_YMLFile load and test tests/test.yml
+func TestRoper_URL(t *testing.T) {
+	doIt("https://raw.githubusercontent.com/applariat/roper/master/labels.yaml", t)
+}
+
+func doIt(file string, t *testing.T) {
+
+	var out = new(TestStruct)
+
+	err := roper.Unmarshal(file, &out)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !reflect.DeepEqual(out, controlTestStruct) {
+		t.Fatal("results don't match")
+	}
+
+}
